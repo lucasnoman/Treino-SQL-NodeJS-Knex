b65b4aad9acfa34285f63b7ed753dfeb942053c1
Lucas Calhau Noman <lucasnoman@LCN-Desktop.local>
2021-01-19 12:10:31 -0300

    procedures and triggers

--- src/database/migrations/20210119110212_create_projects_table.js
+++ src/database/migrations/20210119110212_create_projects_table.js
@@ -1,4 +1,6 @@
-exports.up = knex => knex.schema.createTable('projects', table => {
+const { onUpdateTrigger } = require('../../../knexfile')
+
+exports.up = async knex => knex.schema.createTable('projects', table => {
     table.increments('id')
     table.text('title')
 
@@ -9,6 +11,6 @@ exports.up = knex => knex.schema.createTable('projects', table => {
         .onDelete('CASCADE')
 
     table.timestamps(true, true)
-})
+}).then(() => knex.raw(onUpdateTrigger('projects')))
 
-exports.down = knex => knex.schema.dropTable('projects')
+exports.down = async knex => knex.schema.dropTable('projects')
